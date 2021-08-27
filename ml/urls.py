8f4181a81000cf8229a7c5eabdf9b48ab32c6b8a
asusikai <adj0707@gmail.com>
2021-08-27 04:59:51 +0000

    git update

--- ml/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'myapp'
-urlpatterns = [
-    path(r'', views.index, name='index'),
-    #Add the following(views.py call_write_data()Allows you to send data to)
-    path("ajax/", views.call_write_data, name="call_write_data"),
-]
